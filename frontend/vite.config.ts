228e42d9dc2b5d7f471a5e809609bc9460539005
Kyohei-takiyama <takiyamakyohei@gmail.com>
2024-06-29 15:37:18 +0900

    fix: frontend check gha

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // vitest https://zenn.dev/longbridge/articles/9d9ec773cb3814
+  // vitest
   test: {
     globals: true, // vitest のAPIをImportなしで実行できるようにグローバルにインストール
     environment: 'jsdom', // テスト環境を jsdom に設定
